52fd8d802d4f83d8ef9baa0a182c15aed4e6b346
Murad Shixkmuradov <sixmuradovmurad@gmail.com>
2026-06-04 03:29:04 +0300

    Add exploratory data analysis

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import matplotlib
-import sklearn
-import pandas
+import pandas as pd
 import joblib
+
+df = pd.read_csv("data/creditcard.csv")
+
+print("First 5 rows of credit card data:")
+print(df.head())
+
+print("\nData info:")
+print(df.info())
+
+print("\nStatistics:")
+print(df.describe())
+
+print("\nMissing values:")
+print(df.isnull().sum())
+
+print("\nClass distribution:")
+print(df["Class"].value_counts())
