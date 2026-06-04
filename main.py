44bce5b31503de56aa6a2c15b6e787e734f8513d
Murad Shixkmuradov <sixmuradovmurad@gmail.com>
2026-06-04 03:56:41 +0300

    Add scaler

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv("data/creditcard.csv")
 
@@ -29,9 +30,13 @@ print(y.shape)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1918)
 
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
 print(X_train.shape)
 print(X_test.shape)
 
 model = LogisticRegression(max_iter=1000)
-model.fit(X_train, y_train)
-y_prediction = model.predict(X_test)
+model.fit(X_train_scaled, y_train)
+y_prediction = model.predict(X_test_scaled)
