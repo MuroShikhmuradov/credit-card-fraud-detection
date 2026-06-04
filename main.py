2bef5d3ebe2cdecf6a323a78b256e8c2d0a3481f
Murad Shixkmuradov <sixmuradovmurad@gmail.com>
2026-06-04 03:43:37 +0300

    Add train test split

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import matplotlib
 import pandas as pd
 import joblib
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("data/creditcard.csv")
 
@@ -24,3 +25,8 @@ y = df["Class"]
 
 print(X.shape)
 print(y.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1918)
+
+print(X_train.shape)
+print(X_test.shape)
